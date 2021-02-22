12a3008f4d4ee428135192c0ad2cfd18d7790616
Corey Petty <corey@status.im>
2021-02-22 17:22:59 -0500

    better script update

--- scripts/build_coreth.sh
+++ scripts/build_coreth.sh
@@ -13,7 +13,7 @@ BUILD_DIR="$AVALANCHE_PATH/build" # Where binaries go
 PLUGIN_DIR="$BUILD_DIR/plugins" # Where plugin binaries (namely coreth) go
 BINARY_PATH="$PLUGIN_DIR/evm"
 
-CORETH_VER="1e755bf"
+CORETH_VER="v0.3.25-0.20210222215831-1e755bfe0032"
 
 CORETH_PATH="$GOPATH/pkg/mod/github.com/corpetty/coreth@$CORETH_VER"
 
